4508f607a3b69379450f4d9b366351a3c4ad34b4
Tatyana D <tdorcinvil2022@fau.edu>
2025-01-12 20:19:23 -0600

    Display indiv contribution and total raised + stats for games

--- index.js
+++ index.js
@@ -65,19 +65,30 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((total, game) => total + game.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
 
-// grab the amount raised card, then use reduce() to find the total amount raised
+// grab the amount raised card
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+// use reduce() to calculate the total amount pledged
+const totalRaised = GAMES_JSON.reduce((total, game) => total + game.pledged, 0);
+
+// set the inner HTML using a template literal and toLocaleString to get a number with commas
+raisedCard.innerHTML = `$${totalRaised.toLocaleString()}`;
 
 
-// grab number of games card and set its inner HTML
+// grab the number of games card
 const gamesCard = document.getElementById("num-games");
+
+// calculate the total number of games using the length of the GAMES_JSON array
+const totalGames = GAMES_JSON.length;
+
+// set the inner HTML using a template literal
+gamesCard.innerHTML = `${totalGames}`;
 
 
 /*************************************************************************************
